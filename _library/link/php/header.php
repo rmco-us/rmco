d61dc13f1f227fa232f42c6e000b340ffe507f1c
ninyo <ninyoaganon@gmail.com>
2016-05-16 14:38:05 -0700

    Installed google analytics, waiting for one week to check traffic

--- _library/link/php/header.php
+++ _library/link/php/header.php
@@ -1,16 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Rottweiler Motorcycle Company — <?echo $headtitle;?></title>
+	<?
+	$head_title = "Rottweiler Motorcycle Company";
+	$head_description = "Voted Best Motorcycle Shop in Western WA. All makes, models and years. You deserve the best. Nobody's better at getting you on the road.";
+	?>
+	<title><?echo $head_title;?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	<meta name="description" content="Voted Best Motorcycle Shop in Western WA. All makes, models and years. You deserve the best. Nobody's better at getting you on the road." />
-	<meta property="og:type" content="Company"><meta property="og:locale" content="en_US"><meta property="og:title" content="Rottweiler Motorcycles Company"><meta property="og:description" content="Voted Best Motorcycle Shop in Western WA. All makes, models and years. You deserve the best. Nobody's better at getting you on the road."><meta property="og:image" content="http://i.imgur.com/r3cbj7h.png"><meta property="og:url" content="http://rmco.us">
-	<?//<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">?>
+	<meta name="description" content="<? echo $head_description;?>"/>
+	<meta property="og:type" content="Company">
+	<meta property="og:locale" content="en_US">
+	<meta property="og:title" content="<? echo $head_title;?>">
+	<meta property="og:description" content="<? echo $head_description;?>">
+	<meta property="og:image" content="http://i.imgur.com/r3cbj7h.png">
+	<meta property="og:url" content="http://rmco.us">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="icon" type="image/png" href="_library/img/asset/fav_icon.png">
 	<link rel="stylesheet" type="text/css" href="_library/link/css/rmco.css">
 	<link rel="stylesheet" href="_library/link/css/animate.css">
 	<link href='https://fonts.googleapis.com/css?family=Asap:400,700,400italic' rel='stylesheet' type='text/css'>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-77848604-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 </head>
 <body style="background:#312b29;">
 
